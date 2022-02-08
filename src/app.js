1f6c26d770a477d6eba1787e343305373af7d90f
Arza Aldi K <kampungvector@gmail.com>
2022-02-09 05:12:53 +0700

    add : cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
 const indexRoute = require('./routes/index')
 const imageRoute = require('./routes/image')
 
 const PORT = process.env.PORT || 3001
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 

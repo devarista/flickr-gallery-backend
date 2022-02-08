bd1a039e2998e9d82b710915ceb1fd1631b501ca
Arza Aldi K <kampungvector@gmail.com>
2022-02-09 03:00:59 +0700

    update: /image test

--- __tests__/image.test.js
+++ __tests__/image.test.js
@@ -2,19 +2,15 @@ const app = require('../src/app')
 const request = require('supertest')
 
 describe('Test all /images methods', () => {
-    it('get /image ==> should return 200 and array of images', () => {
-        request(app).get('/image').set('Accept', 'application/json').expect('Content-Type', /json/).expect(200)
+    it('GET /image ===> 200, array of images', () => {
+        return request(app).get('/image').expect('Content-Type', /json/).expect(200)
     })
 
-    it('get /image?tags=cat,dog (with query) ==> should return 200 and array of images', () => {
-        request(app)
-            .get('/image?tags=cat,dog')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then((res) => {
-                expect(res.body.title).toEqual('Recent Uploads tagged cat and dog')
-            })
-            .catch((err) => err)
+    it('GET /image?tags=cat ===> 200, array of images', () => {
+        return request(app).get('/image?tags=cat').expect('Content-Type', /json/).expect(200)
+    })
+
+    it('GET /image?tags=randomstringwillnotwork ===> 404', () => {
+        return request(app).get('/image?tags=kalsjlkgdhjskldjf').expect('Content-Type', /json/).expect(404)
     })
 })

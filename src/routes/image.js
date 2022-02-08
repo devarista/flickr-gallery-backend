823fb2cedeca575197fbb4fbc172e5e756893d76
Arza Aldi K <kampungvector@gmail.com>
2022-02-09 03:03:10 +0700

    update : return 404 if items doesn't exist

--- src/routes/image.js
+++ src/routes/image.js
@@ -4,13 +4,13 @@ const router = express.Router()
 
 const url = process.env.FLICKR_URL
 
-router.get('/images', async (req, res) => {
+router.get('/', async (req, res) => {
     const { tags } = req.query
     const includeTags = tags ? `&tags=${tags}` : ''
 
     const { data } = await axios.get(`${url}${includeTags}`)
 
-    if (!data.items) return res.status(404).json({ message: 'No Entry' })
+    if (data.items.length === 0) return res.status(404).json({ message: 'No Entry' })
 
     return res.status(200).json(data)
 })
